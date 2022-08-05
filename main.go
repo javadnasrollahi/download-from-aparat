7845dcc6f364d5a3e1d3c1982ab3924ff7f98efd
Javad Nasrollahi <47215007+javadnasrollahi@users.noreply.github.com>
2022-08-06 02:30:24 +0430

    init

---
+++ main.go
@@ -0,0 +1,77 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+)
+
+type response struct {
+	Data data `json:"data,omitempry"`
+}
+type data struct {
+	Attributes attributes `json:"attributes,omitempry"`
+}
+type attributes struct {
+	File_link_all []file_link_all `json:"file_link_all,omitempry"`
+}
+type file_link_all struct {
+	Text    string   `json:"text,omitempry"`
+	Profile string   `json:"profile,omitempry"`
+	Urls    []string `json:"urls,omitempry"`
+}
+
+func download(name, url string) {
+	out, err := os.Create("files/" + name + ".mp4")
+	if err != nil {
+		fmt.Println("download Create", err)
+		return
+	}
+	defer out.Close()
+	resp, err := http.Get(url)
+	if err != nil {
+		fmt.Println("download Get", err)
+		return
+	}
+	defer resp.Body.Close()
+	_, err = io.Copy(out, resp.Body)
+	if err != nil {
+		fmt.Println("download Copy", err)
+		return
+	}
+}
+func main() {
+	// list of data-uid
+	// https://www.aparat.com/v/7dmXg =====>  7dmXg
+	listToken := []string{
+		"7dmXg", // https://www.aparat.com/v/7dmXg =====>  7dmXg
+	}
+
+	for i := range listToken {
+		fmt.Println(listToken[i])
+		res, err := http.Get("https://www.aparat.com/api/fa/v1/video/video/show/videohash/" + listToken[i] + "?pr=1&mf=1")
+		if err != nil {
+			fmt.Println("Get", err)
+			continue
+		}
+		defer res.Body.Close()
+		var item response
+		err = json.NewDecoder(res.Body).Decode(&item)
+		if err != nil {
+			fmt.Println("NewDecoder", err)
+			continue
+		}
+		//
+		if len(item.Data.Attributes.File_link_all) > 0 && len(item.Data.Attributes.File_link_all[len(item.Data.Attributes.File_link_all)-1].Urls) > 0 {
+			l := len(item.Data.Attributes.File_link_all)
+			u := len(item.Data.Attributes.File_link_all[len(item.Data.Attributes.File_link_all)-1].Urls)
+			url := item.Data.Attributes.File_link_all[l-1].Urls[u-1]
+			if len(url) > 0 {
+				// It downloads the best quality
+				download(listToken[i], url)
+			}
+		}
+	}
+}
